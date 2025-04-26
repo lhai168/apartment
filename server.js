0905909071e153febbeeaeb2cbd14d738330ee1e
lhai168 <53025841+lhai168@users.noreply.github.com>
2025-04-26 16:21:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended: true}));
-app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+app.use(cors({credentials: true, origin: "https://apartment-puoq.onrender.com""}));
 
 
 
